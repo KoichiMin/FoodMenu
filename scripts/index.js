cd8ecc0a71c2926c1043bcbe22b6cbf994421b7a
KoichiMin <koichidminami@gmail.com>
2023-11-02 13:47:32 -0400

    finished exercise

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+"use strict"
+
+let menu = {
+    drinks : [
+    "Water", "Tea", "Sweet Tea",
+    "Coke", "Dr. Pepper", "Sprite"
+    ],
+    entrees : [
+    "Hamburger w/ Fries",
+    "Grilled Cheese w/ Tater Tots",
+    "Grilled Chicken w/ Veggies",
+    "Chicken Fried Steak w/ Mashed Potatoes",
+    "Fried Shrimp w/ Coleslaw",
+    "Veggie Plate"
+    ],
+    desserts: [
+    "Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"
+    ]
+    };
+
+
+window.onload = function(){
+    const menuSelect = document.getElementById('menuSelect');
+    const menuContent = document.getElementById('menuContent');
+    
+    menuSelect.addEventListener('change', function() {
+        menuContent.innerHTML = ""
+        const selectedCategory = menuSelect.value;
+        
+        // display select tag if the value is not empty
+        if(selectedCategory){
+            menuContent.style.display = "block"
+        } else{
+            menuContent.style.display = "none"
+        }
+
+        for(let selectedMealOption of menu[selectedCategory]){
+            let optionTag = document.createElement("option");
+            optionTag.textContent = selectedMealOption
+            menuContent.appendChild(optionTag)
+        }
+    });
+
+}
+
+
+
+
